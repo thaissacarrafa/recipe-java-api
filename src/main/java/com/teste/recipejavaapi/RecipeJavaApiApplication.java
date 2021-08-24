ca4a72f7e2dd55b214351d3d7d3c4fbae2af81b5
Thaissa Carrafa <thaissacarrafa@gmail.com>
2021-08-23 23:01:38 -0300

    Aplication Startup

---
+++ src/main/java/com/teste/recipejavaapi/RecipeJavaApiApplication.java
@@ -0,0 +1,14 @@
+package com.teste.recipejavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeJavaApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RecipeJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/recipejavaapi/RecipeJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.recipejavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
