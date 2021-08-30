0abd77e626171bf27ecf46124e515fe34f49a8d1
Thaissa Carrafa <thaissacarrafa@gmail.com>
2021-08-30 07:58:34 -0300

    Adding Service and ServiceImpl

--- src/main/java/com/teste/recipejavaapi/service/RecipeService.java
+++ src/main/java/com/teste/recipejavaapi/service/RecipeService.java
@@ -2,44 +2,17 @@ package com.teste.recipejavaapi.service;
 
 import java.util.List;
 
-/**
- * Interface que define os metodos relacionados aos fluxos de receita
- */
-public interface ReceitasService{
+public interface RecipeService {
 
-    /**
-     * Método que retornar todas as receitas cadastras
-     * @return List<recipe>
-     */
-    List<recipe> listarReceitas();
+    Recipe save(Recipe recipe);
 
-    /**
-     * Metodo que ç cria uma nova receita
-     * @param recipe
-     */
-    void adicionarReceita(Recipe recipe);
+    void update(String id, Recipe recipe);
 
-    /**
-     * metodo que altera uma receita ja cadastrada
-     * @param id
-     * @param recipe
-     */
-    void alterarReceita(String id, Recipe recipe);
+    void delete(String id);
 
-    /**
-     * metodo que exclui uma receita cadastra
-     * @param id
-     */
-    void excluirReceita(String id);
+    Recipe get(String id);
 
-    /**
-     * * método que excluir um ingrediente
-     */
-    * @param ingredient;
-    void excluirIngredient (Integer ingredient);
-    /**
-     * metodo que obtem uma receita cadastra
-     * @param id
-     */
-    Recipe obterReceita(String id);
+    List<Recipe> listByIngredient(String ingredient);
+
+
 }
